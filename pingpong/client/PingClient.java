63e9e1cfb84578f8952d8f1f083bbc19f8c64faf
Adam C Setters <asetters@ucsd.edu>
2017-02-15 18:14:41 -0800

    pingy pongy doney

--- pingpong/client/PingClient.java
+++ pingpong/client/PingClient.java
@@ -6,17 +6,36 @@ import java.net.InetSocketAddress;
 import rmi.Skeleton;
 import rmi.Stub;
 
+
 public class PingClient {
+    private static final int NUM_TESTS = 4;
 	public static void main (String args[]) {
 		InetSocketAddress address = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
+        PingServer ps = null;
+        int count = 0;
 		try {
 			PingServerFactory f = Stub.create(PingServerFactory.class,
 										      address);
 
-			PingServer ps = f.makePingServer();
+			ps = f.makePingServer();
+            if(ps != null) {
+                for(int i = 0; i < NUM_TESTS; ++i) {
+                    String str = ps.ping(i);
+                        if(str.equals("Pong " + i)) {
+                            count++;
+                        }
+                }
+            } else {
+                System.err.println("\n\t\tFailed to retreive stub for PingServer...");
+            }
 		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            System.err.println("Exception occurred");
+		} finally {
+            System.out.println(count + " Tests Completed, " 
+                + (NUM_TESTS - count) + " Tests Failed.");
+
+        }
+                
 
 
 
